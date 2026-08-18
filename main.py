c8e171dfca39694a53f44ef088ed8ae255d824eb
Usuário Local Riacho Fundo <acessolocal@ifb.local>
2026-08-18 15:26:34 -0300

    funcao listar eventos

--- main.py
+++ main.py
@@ -22,6 +22,15 @@ def adicionarEvento(listaEventos, nome, data, local, categoria, cidade):
     listaEventos.append(novoEvento)
 
 
+def listar_eventos(listaDeEvento):
+    """Lista os eventos cadatrados.
+    """
+    print("\n--- Lista de Eventos ---")
+    for novoEvento in listaDeEvento:
+        if novoEvento[2] > 0:                   
+            print(f"Nome: {novoEvento[0]} | data: R$ {novoEvento[1]} | local: {novoEvento[3]} | categoria: {novoEvento[4]} | cidade: {novoEvento[5]}")
+        
+
 def displayMenu():
     print("=== Planejador de Eventos do Campus ===")
     print("1. Adicionar Evento")
