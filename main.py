1dac30091c9be66d82f97462be5bdd7ac9fd0e01
Usuário Local Riacho Fundo <acessolocal@ifb.local>
2026-08-18 16:43:04 -0300

    funcao filtrar

--- main.py
+++ main.py
@@ -29,6 +29,22 @@ def listar_eventos(listaDeEvento):
         if novoEvento[2] > 0:                   
             print(f"Nome: {novoEvento[0]} | data: R$ {novoEvento[1]} | local: {novoEvento[3]} | categoria: {novoEvento[4]} | cidade: {novoEvento[5]}")
         
+def filtrar_eventos(listaDeEvento):
+    
+    nome_busca = input("Digite o nome (ou parte dele) para buscar: ")
+    encontrou = False
+    
+    print("\n--- RESULTADO DA BUSCA ---")
+    for novoEvento in listaDeEvento:
+       
+        if nome_busca.lower() in novoEvento[0].lower():
+            print(f"Nome: {novoEvento[0]} | data: R$ {novoEvento[1]} | local: {novoEvento[3]} | categoria: {novoEvento[4]} | cidade: {novoEvento[5]}")
+            encontrou = True
+            
+    if not encontrou:
+        print("Nenhum produto encontrado com esse termo.")    
+
+
 
 def displayMenu():
     print("=== Planejador de Eventos do Campus ===")
@@ -38,8 +54,7 @@ def displayMenu():
     print("4. Marcar Evento como Participado")
     print("5. Gerar Relatório")
     print("6. Sair")
-
-
+    
 def main():
     while True:
         displayMenu()
