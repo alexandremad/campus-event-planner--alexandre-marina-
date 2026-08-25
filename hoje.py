831913aa938be65a91d7553720ed036fc828fa4b
Usuário Local Riacho Fundo <acessolocal@ifb.local>
2026-08-25 14:52:54 -0300

    adicionando mais linhas

--- hoje.py
+++ hoje.py
@@ -1,1 +1,8 @@
 Por que gatos miam para você abrir a porta, mas não entram quando você abre?
+
+Gatos são animais territorialistas e altamente sensíveis a mudanças no ambiente. Uma porta fechada representa uma barreira que limita a visão e o acesso ao território, 
+
+
+Por que gatos miam para você abrir a porta, mas não entram quando você abre?
+
+Gatos são animais territorialistas e altamente sensíveis a mudanças no ambiente. Uma porta fechada representa uma barreira que limita a visão e o acesso ao território, 
